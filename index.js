30c6c19f355e0859d375f3f4f4c889b60d282bfc
derrick b <dbozich1@gmail.com>
2018-09-24 05:39:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ class Meal {
     store.meals.push(this);
   }
   deliveries(){
-    return store.deliveries.filter(delivery =>{
+    return store.deliveries.filter(delivery => {
       delivery.mealId === this.id;
     });
   }
