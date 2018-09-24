bdb9bd16e18caa351a451357d3e019f1c5a809eb
derrick b <dbozich1@gmail.com>
2018-09-24 04:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0;
+let mealId = 0;
 
 class Neighborhood {
   constructor(name) {
     this.name = name;
     this.id = ++neighborhoodId;
+  }
+}
+
+class Meal {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.id = ++mealId;
   }
 }
