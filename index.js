f0041717b572759592b27465bc250b050d1dc541
derrick b <dbozich1@gmail.com>
2018-09-24 05:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,10 +40,11 @@ class Meal {
     let deliveries = [];
     store.deliveries.forEach(delivery => {
       let meal = store.meals.find(meal => meal.id === this.id);
-      debugger
+
       if (meal.title === this.title && (!(deliveries.includes(delivery)))) {
           deliveries.push(delivery);
       }
+        debugger
     });
     return deliveries;
   }
